7a220e6db293ba800abc5289abec96d797016a2b
lotarv <lotarev200445@gmail.com>
2024-05-12 13:06:53 +0300

    Quizworks!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.scss';
-
+import { useState, useRef} from 'react';
 const questions = [
   {
     title: 'React - это ... ?',
@@ -17,42 +17,60 @@ const questions = [
       'Это простой HTML',
       'Это функция',
       'Это тот же HTML, но с возможностью выполнять JS-код',
+      
     ],
     correct: 2,
   },
 ];
 
-function Result() {
+function Result({correctAnswers, setStep}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>Вы отгадали {correctAnswers.current} ответов из {questions.length}</h2>
+      <button onClick={() => {
+        setStep(0);
+        correctAnswers.current = 0;
+        }}>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+
+function Game({question, correctAnswers, setStep, step}) {
+
+  function checkAnswer(i_question, i_correct){
+    if (i_question == i_correct){
+      correctAnswers.current++;
+    }
+    setStep(c => c + 1);
+  }
+
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${(step + 1) * 100 / questions.length}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((variant, index) => 
+          <li key = {index} onClick = {() => {checkAnswer(index, question.correct)}}>{variant}</li>
+        )}
       </ul>
     </>
   );
 }
 
 function App() {
+
+  const [step, setStep] = useState(0);
+  const question = questions[step];
+  const correctAnswers = useRef(0);
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      { step <= questions.length -1 && <Game step = {step} setStep = {setStep}question={question} correctAnswers = {correctAnswers}/>}
+      {step > questions.length - 1 && <Result correctAnswers = {correctAnswers} setStep = {setStep} />}
     </div>
   );
 }
